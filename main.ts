fe0c5b3c0cc0720f0ae6a1997ae29f8a3a496731
pacobaor <98743296+pacobaor@users.noreply.github.com>
2022-03-28 11:24:43 +0200

    deycgd

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 basic.forever(function () {
-    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
-    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 20)
     }
